0e684f8e2ee98cbb0da963d61be0e8d06c770d1c
poiug07 <kozhin.asan@gmail.com>
2022-01-30 07:53:57 +0800

    Added loading model page

--- predict/urls.py
+++ predict/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login_user, name='login'),
     path('app', views.app, name='app'),
+    path('loading/<int:id>', views.loading, name='loading'),
     path("logout/", views.logout_user, name="logout"),
-    path('get_data/<key_id>/', views.get_data, name='key_id'),
-    path("graph/", views.graph, name="graph"),
+    path('get_data/<int:key_id>/', views.get_data, name='key_id'),
+    path("graph/<int:datafile_id>", views.graph, name="graph"),
 ]
