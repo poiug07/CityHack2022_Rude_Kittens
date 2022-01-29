e0c7e8c725da935da319fce8c1d089b7e9abe320
poiug07 <kozhin.asan@gmail.com>
2022-01-29 20:24:38 +0800

    Fixed form

--- predict/forms.py
+++ predict/forms.py
@@ -2,6 +2,20 @@ from django import forms
 from predict.models import DataFile
 
 class FileForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "class":"form-control",
+            "placeholder": "File Name",
+            "aria-label": "Filename",
+            "aria-describedby": "addon-wrapping"
+        }
+    ))
+    filepath = forms.FileField(widget=forms.FileInput(
+        attrs={
+            "class": "form-control",
+        }
+    ))
+
     class Meta:
         model = DataFile
         fields = ["name", "filepath"]
