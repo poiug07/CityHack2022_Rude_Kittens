080c7b3a6b8c17b4ae589bff3e9544ccd4fb1c15
developer1 <developer1@altama.co.id>
2022-01-29 21:11:18 +0800

    added read excel support

--- predict/model_predict.py
+++ predict/model_predict.py
@@ -1,12 +1,19 @@
 import asyncio
 import time
 from predict import models
+from openpyxl import load_workbook
+import pandas as pd
 
-def predict_data(job_id):
-    # Run model here
+def predict_data(job_id):    
     # open excel file
-
+    d = models.DataFile.objects.get(id=job_id)
+    wb = load_workbook(d.filepath)
     # convert to pandas dataframe
-    
+    ws = wb.worksheets[0]
+    data = ws.values
+    columns = next(data)[0:]
+    df = pd.DataFrame(data, columns=columns)
+    print(df)
+
     models.RunningJobs.objects.filter(datafile_id=job_id).delete()
     return 0
