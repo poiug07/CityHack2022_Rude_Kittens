caaf7b91dd07c78bd9a88f7f9df26c7dcd02ceca
poiug07 <kozhin.asan@gmail.com>
2022-01-29 13:53:13 +0800

    Fixed some html

--- predict/urls.py
+++ predict/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
+    path('login', views.login_user, name='login'),
     path('app', views.app, name='app'),
     path('predict', views.predict, name='predict'),
-    path('add-xlsx', views.upload_xlsx, name='upload_data'),
+    path('add-xlsx', views.add_xlsx, name='upload_data'),
     path("login/", views.login_user, name="login"),
 ]
