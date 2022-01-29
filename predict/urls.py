1e498048dd6720c335dc5913d39b4953b473ef58
poiug07 <kozhin.asan@gmail.com>
2022-01-30 07:32:35 +0800

    Combined app and add-xlsx

--- predict/urls.py
+++ predict/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login_user, name='login'),
     path('app', views.app, name='app'),
-    path('add-xlsx', views.add_xlsx, name='upload_data'),
     path("logout/", views.logout_user, name="logout"),
     path('get_data/<key_id>/', views.get_data, name='key_id'),
     path("graph/", views.graph, name="graph"),
